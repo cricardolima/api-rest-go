70a03de291e7cf402adab3bc3f4f39ec18a938f3
Ricardo Lima <ricardo.engp@gmail.com>
2022-10-06 20:07:22 -0300

    feat: API completed

--- routes/routes.go
+++ routes/routes.go
@@ -2,6 +2,8 @@ package routes
 
 import (
 	"api-rest-go/controllers"
+	"api-rest-go/middleware"
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
@@ -9,6 +11,7 @@ import (
 
 func HandleRequest() {
 	r := mux.NewRouter()
+	r.Use(middleware.ContentTypeMiddleware)
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/personalities", controllers.GetAll).Methods("Get")
 	r.HandleFunc("/api/personalities/{id}", controllers.GetOneById).Methods("Get")
@@ -16,5 +19,5 @@ func HandleRequest() {
 	r.HandleFunc("/api/personalities/{id}", controllers.Delete).Methods("Delete")
 	r.HandleFunc("/api/personalities/{id}", controllers.Update).Methods("Put")
 
-	log.Fatal(http.ListenAndServe(":8000", r))
+	log.Fatal(http.ListenAndServe(":8000", handlers.CORS(handlers.AllowedOrigins([]string{"*"}))(r)))
 }
